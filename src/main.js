18f4ac70938fa55ab093efecaba7e09cd7cc56b5
nextgensparx <siphomateke@gmail.com>
2017-09-04 21:54:59 +0200

    Add unload event

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 
+window.addEventListener('unload', () => {
+  chrome.runtime.sendMessage({
+    from: 'app',
+    type: 'loadEvent',
+    loadState: 'unload'
+  })
+});
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
