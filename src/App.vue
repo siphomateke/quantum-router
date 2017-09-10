55e0d56e5b085568b351e6d6312ac5280d260f52
nextgensparx <siphomateke@gmail.com>
2017-09-10 17:39:33 +0200

    Beautify, add sms count

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
-<div id="app">
-  <div class="app-wrapper columns is-gapless">
-    <app-drawer :title="drawer.title" :items="drawer.items" class="column is-2"></app-drawer>
-    <div class="column">
-      <q-toolbar :items="toolbar.items">
-        <template slot="toolbar-end">
-          <q-toolbar-item icon="bell" link="sms">
-            <div class="badge">3</div>
-          </q-toolbar-item>
-        </template>
-      </q-toolbar>
-      <div class="page-wrapper">
-        <router-view></router-view>
+  <div id="app">
+    <div class="app-wrapper columns is-gapless">
+      <app-drawer :title="drawer.title" :items="drawer.items" class="column is-2"></app-drawer>
+      <div class="column">
+        <q-toolbar :items="toolbar.items">
+          <template slot="toolbar-end">
+            <q-toolbar-item icon="bell" link="sms">
+              <div class="badge">{{ smsCount }}</div>
+            </q-toolbar-item>
+          </template>
+        </q-toolbar>
+        <div class="page-wrapper">
+          <router-view></router-view>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -25,54 +25,60 @@ import Drawer from '@/components/Drawer.vue';
 import Navbar from '@/components/Navbar.vue';
 import Toolbar from '@/components/Toolbar.vue';
 import ToolbarItem from '@/components/ToolbarItem.vue';
+import { RouterController } from './chrome/router.js';
 
 Vue.use(Buefy, {
   defaultIconPack: 'fa'
 });
 export default {
   name: 'app',
+  components: {
+    'app-drawer': Drawer,
+    'b-navbar': Navbar,
+    'q-toolbar': Toolbar,
+    'q-toolbar-item': ToolbarItem
+  },
   data() {
     return {
+      refreshInterval: 1000,
+      smsCount: 0,
       drawer: {
         title: 'Quantum Router',
         items: [{
-            link: 'home',
-            label: 'Home',
-            icon: 'home'
-          },
-          {
-            link: 'sms',
-            label: 'SMS',
-            icon: 'comment'
-          },
-          {
-            link: 'statistics',
-            label: 'Statistics',
-            icon: 'pie-chart'
-          },
-          {
-            link: 'ussd',
-            label: 'USSD',
-            icon: 'terminal'
-          },
-          {
-            link: 'settings',
-            label: 'Settings',
-            icon: 'cog'
-          }
+          link: 'home',
+          label: 'Home',
+          icon: 'home'
+        },
+        {
+          link: 'sms',
+          label: 'SMS',
+          icon: 'comment'
+        },
+        {
+          link: 'statistics',
+          label: 'Statistics',
+          icon: 'pie-chart'
+        },
+        {
+          link: 'ussd',
+          label: 'USSD',
+          icon: 'terminal'
+        },
+        {
+          link: 'settings',
+          label: 'Settings',
+          icon: 'cog'
+        }
         ]
       },
       toolbar: {
-        items:[
-          {id: 0, icon:'bell'},
-          {id: 1, icon: 'plug'},
-          {id: 2, icon: 'wifi'},
+        items: [
+          { id: 0, icon: 'bell' },
+          { id: 1, icon: 'plug' },
+          { id: 2, icon: 'wifi' },
         ]
       }
     }
-  },
-  methods: {
-
   },
   mounted() {
     chrome.runtime.sendMessage({
@@ -80,18 +86,22 @@ export default {
       type: 'loadEvent',
       loadState: 'load'
     });
+    this.refresh(0);
   },
-  components: {
-    'app-drawer': Drawer,
-    'b-navbar': Navbar,
-    'q-toolbar': Toolbar,
-    'q-toolbar-item': ToolbarItem
+  methods: {
+    refresh() {
+      RouterController.getSmsCount().then(response => {
+        if (response.type === 'response') {
+          this.smsCount = response.data.LocalUnread;
+        }
+      });
+      setTimeout(this.refresh, this.refreshInterval);
+    }
   }
 }
 </script>
 
 <style lang="scss">
-
 @import "~styles/vars";
 
 // Import Bulma and Buefy styles
@@ -99,11 +109,11 @@ export default {
 @import "~buefy/src/scss/buefy";
 @import "~styles/bulma-extensions";
 
-body{
+body {
   background: $page-background-color;
 }
 
-.page-wrapper{
+.page-wrapper {
   position: relative;
 }
 
