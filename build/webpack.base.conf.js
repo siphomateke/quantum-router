68317f59a424aba797f18b0815b1889af13129f7
nextgensparx <siphomateke@gmail.com>
2017-09-11 10:59:40 +0200

    Lint and use utils.resolve

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,66 +1,62 @@
-var path = require('path');
-var utils = require('./utils');
-var config = require('../config');
-var vueLoaderConfig = require('./vue-loader.conf');
-
-function resolve(dir) {
-  return path.join(__dirname, '..', dir);
-}
+let path = require('path');
+let utils = require('./utils');
+let config = require('../config');
+let vueLoaderConfig = require('./vue-loader.conf');
 
 module.exports = {
   entry: {
-    'app': resolve('src/main.js'),
-    'background': resolve('src/chrome/background.js'),
-    'content': resolve('src/chrome/content.js')
+    'app': utils.resolve('src/main.js'),
+    'background': utils.resolve('src/chrome/background.js'),
+    'content': utils.resolve('src/chrome/content.js'),
   },
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
     publicPath: process.env.NODE_ENV === 'production' ?
-      config.build.assetsPublicPath : config.dev.assetsPublicPath
+      config.build.assetsPublicPath : config.dev.assetsPublicPath,
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      '@': resolve('src'),
-      'styles': resolve('src/styles')
-    }
+      '@': utils.resolve('src'),
+      'styles': utils.resolve('src/styles'),
+    },
   },
   module: {
     rules: [{
       test: /\.vue$/,
       loader: 'vue-loader',
-      options: vueLoaderConfig
+      options: vueLoaderConfig,
     },
     {
       test: /\.js$/,
       loader: 'babel-loader',
-      include: [resolve('src'), resolve('test')]
+      include: [utils.resolve('src'), utils.resolve('test')],
     },
     {
       test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
       loader: 'url-loader',
       options: {
         limit: 10000,
-        name: utils.assetsPath('img/[name].[hash:7].[ext]')
-      }
+        name: utils.assetsPath('img/[name].[hash:7].[ext]'),
+      },
     },
     {
       test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
       loader: 'url-loader',
       options: {
         limit: 10000,
-        name: utils.assetsPath('media/[name].[hash:7].[ext]')
-      }
+        name: utils.assetsPath('media/[name].[hash:7].[ext]'),
+      },
     },
     {
       test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
       loader: 'url-loader',
       options: {
         limit: 10000,
-        name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-      }
-    }
-    ]
-  }
+        name: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
+      },
+    },
+    ],
+  },
 };
