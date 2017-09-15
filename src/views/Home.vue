d724526a7d153efe4b0224f537a6394cbca47883
nextgensparx <siphomateke@gmail.com>
2017-09-15 13:33:45 +0200

    Add received/sent messages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,34 @@
 <template>
 <div class="page-content">
-
+  <div class="box">
+    <section class="section">
+      <div class="content">
+        Received/Sent: {{ received }} / {{ sent }}
+      </div>
+    </section>
+  </div>
 </div>
 </template>
 
 <script>
- export default {
-   name: 'Home',
-   data() {
-     return { };
-   },
- };
+import {RouterController} from '@/chrome/router.js';
+export default {
+  name: 'Home',
+  data() {
+    return {
+      received: 0,
+      sent: 0,
+    };
+  },
+  mounted() {
+    this.bus.$on('refresh', () => {
+      RouterController.getTrafficStatistics().then((data) => {
+        this.received = data.CurrentDownload;
+        this.sent = data.CurrentUpload;
+      });
+    });
+  },
+};
 </script>
 
 <style lang="scss" scoped>
