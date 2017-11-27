04c396ddbc933d3d7e7d38a6bd1761e8caed94fa
Sipho Mateke <siphomateke@gmail.com>
2017-11-27 12:28:21 +0200

    Show message when there are no notifications

--- src/components/notifications/NotificationList.vue
+++ src/components/notifications/NotificationList.vue
@@ -1,12 +1,23 @@
 <template>
 <div class="q-notification-list" style="width:400px;max-height:500px;overflow-y:scroll;">
-  <template v-for="(n, key) in list">
-    <notification :key="key"
-    :title="n.title"
-    :message="n.message"
-    :date="n.date"
-    :progress="n.progress"
-    ></notification>
+  <template v-if="list.length>0">
+    <template v-for="(n, key) in list">
+      <notification :key="key"
+      :title="n.title"
+      :message="n.message"
+      :date="n.date"
+      :progress="n.progress"
+      ></notification>
+    </template>
+  </template>
+  <template class="content" v-else>
+    <section class="section">
+      <div class="content has-text-grey has-text-centered">
+        <p>
+          No notifications
+        </p>
+      </div>
+    </section>
   </template>
 </div>
 </template>
