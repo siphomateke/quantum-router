f323459c5b34d7491410adbb096a52496ec568ba
nextgensparx <siphomateke@gmail.com>
2017-12-12 19:45:44 +0200

    Remove unused code

--- src/components/sms/SmsList.vue
+++ src/components/sms/SmsList.vue
@@ -1,57 +1,52 @@
 <template>
-<div class="sms-list">
-  <b-table
-  :data="list"
-  :bordered="false"
-  :striped="true"
-  :narrowed="true"
-  :loading="loading"
-  :mobile-cards="true"
-  :checked-rows.sync="checkedRows"
-  checkable
+  <div class="sms-list">
+    <b-table
+    :data="list"
+    :bordered="false"
+    :striped="true"
+    :narrowed="true"
+    :loading="loading"
+    :mobile-cards="true"
+    :checked-rows.sync="checkedRows"
+    checkable
 
-  paginated
-  backend-pagination
-  :total="total"
-  :per-page="perPage"
-  @page-change="onPageChange"
+    paginated
+    backend-pagination
+    :total="total"
+    :per-page="perPage"
+    @page-change="onPageChange"
 
-  backend-sorting
-  :default-sort-direction="sortOrder"
-  :default-sort="['date', sortOrder]"
-  @sort="onSort">
-    <template slot-scope="props">
-        <b-table-column field="read">
-          <b-icon :icon="props.row.read ? 'envelope-open-o' : 'envelope'"></b-icon>
-        </b-table-column>
-        <b-table-column label="Number" field="number">{{ props.row.number }}</b-table-column>
-        <b-table-column label="Content" field="content">
-          <div class="content">{{ props.row.content }}</div>
-        </b-table-column>
-        <b-table-column field="date" label="Date" sortable centered>
-          <span style="white-space:nowrap;">{{ formatDate(props.row.date) }}</span>
-        </b-table-column>
-    </template>
+    backend-sorting
+    :default-sort-direction="sortOrder"
+    :default-sort="['date', sortOrder]"
+    @sort="onSort">
+      <template slot-scope="props">
+          <b-table-column field="read">
+            <b-icon :icon="props.row.read ? 'envelope-open-o' : 'envelope'"></b-icon>
+          </b-table-column>
+          <b-table-column label="Number" field="number">{{ props.row.number }}</b-table-column>
+          <b-table-column label="Content" field="content">
+            <div class="content">{{ props.row.content }}</div>
+          </b-table-column>
+          <b-table-column field="date" label="Date" sortable centered>
+            <span style="white-space:nowrap;">{{ formatDate(props.row.date) }}</span>
+          </b-table-column>
+      </template>
 
-    <template slot="empty">
-      <section class="section">
-        <template v-if="!loading">
-          <p>There are no SMSs</p>
-        </template>
-      </section>
-    </template>
-  </b-table>
-</div>
+      <template slot="empty">
+        <section class="section">
+          <template v-if="!loading">
+            <p>There are no SMSs</p>
+          </template>
+        </section>
+      </template>
+    </b-table>
+  </div>
 </template>
 
 <script>
 import moment from 'moment';
 export default {
-  data() {
-    return {
-
-    };
-  },
   props: {
     'list': Array,
     'checked-rows': Array,
@@ -76,7 +71,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" scoped>
-  @import '~styles/vars.scss';
-</style>
