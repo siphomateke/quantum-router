337c952e64f3543515295581c447a9f56d81fb37
nextgensparx <siphomateke@gmail.com>
2018-01-05 17:29:20 +0200

    Move error comment to error file

--- src/router/controller.js
+++ src/router/controller.js
@@ -7,23 +7,6 @@ import moment from 'moment';
 
 /* global chrome */
 
-/* const errors = [
-  'xhr_error',
-  'xhr_invalid_xml',
-  'xhr_invalid_status',
-  'xml_type_invalid',
-  'xml_response_not_ok',
-  'tabs_not_found',
-  'chrome_runtime_message_error',
-  'chrome_tabs_message_error',
-  'router_url_not_set',
-  'chrome_storage_error',
-  'invalid_router_url',
-  'xhr_timeout',
-  'ussd_timeout',
-  'ussd_release_fail',
-];*/
-
 /**
  * Controls access to the router
  */
