4227e2e23d39414c8c1c20878637e6f159157c79
nextgensparx <siphomateke@gmail.com>
2018-08-29 23:07:44 +0200

    Remove extension background script

--- src/browser/background.js
+++
@@ -1,5 +0,0 @@
-/* global browser */
-browser.browserAction.onClicked.addListener(() => {
-  // TODO: Show error message if tab can not be opened
-  browser.tabs.create({url: browser.extension.getURL('../index.html')});
-});
