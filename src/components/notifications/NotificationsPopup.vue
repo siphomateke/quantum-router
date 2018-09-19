771788c195f22fba05f9abf314612ad0aba7e1dc
nextgensparx <siphomateke@gmail.com>
2018-09-19 13:20:29 +0200

    Tidy up styles
    - Don't prefix class names unless necessary
    - Add class for text after icons

--- src/components/notifications/NotificationsPopup.vue
+++ src/components/notifications/NotificationsPopup.vue
@@ -35,20 +35,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .notification-popup{
-    .popup-card{
-      .popup-card-head{
-        border-bottom: 1px solid #dbdbdb;
-        border-top-left-radius: 5px;
-        border-top-right-radius: 5px;
-        background-color: #f5f5f5;
-        padding: 15px;
-        .popup-card-title{
-          color: #363636;
-          font-weight: bold;
-          line-height: 1;
-        }
-      }
-    }
+.notification-popup .popup-card .popup-card-head{
+  border-bottom: 1px solid #dbdbdb;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  background-color: #f5f5f5;
+  padding: 15px;
+  .popup-card-title{
+    color: #363636;
+    font-weight: bold;
+    line-height: 1;
   }
+}
 </style>
