4d75e5868515f805d55aa3bb915d954616faf379
nextgensparx <siphomateke@gmail.com>
2018-09-21 15:50:34 +0200

    Move icon list into separate component

---
+++ src/components/iconpicker/IconPickerIconList.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="icon-list">
+    <button
+      v-for="icon in visibleIcons"
+      :key="icon.id"
+      :class="{'is-primary': icon.id === value.id, 'is-transparent': icon.id !== value.id}"
+      :title="icon.name"
+      type="button"
+      class="button"
+      @click="select(icon)">
+      <b-icon
+        :pack="iconPack.id"
+        :icon="icon.id"/>
+    </button>
+  </div>
+</template>
+
+<script>
+import { selectedIcon } from './props';
+
+export default {
+  props: {
+    iconPack: {
+      type: Object,
+      default: null,
+      validator: value => 'icons' in value && 'id' in value && 'name' in value,
+    },
+    search: {
+      type: String,
+      default: '',
+    },
+    value: selectedIcon,
+  },
+  computed: {
+    searchLowerCase() {
+      return this.search.toLowerCase();
+    },
+    visibleIcons() {
+      const { icons } = this.iconPack;
+      if (this.search && this.search.length > 0) {
+        return icons.filter((icon) => {
+          const toMatch = ['id', 'name', 'filter', 'categories'];
+          for (const prop of toMatch) {
+            if (icon[prop]) {
+              if (typeof icon[prop] === 'string' && icon[prop].toLowerCase().includes(this.searchLowerCase)) {
+                return true;
+              } else if (Array.isArray(icon[prop])) {
+                const regex = new RegExp(this.searchLowerCase);
+                const match = regex.test(icon[prop].join('|'));
+                if (match) {
+                  return true;
+                }
+              }
+            }
+          }
+          return false;
+        });
+      }
+      return icons;
+    },
+  },
+  methods: {
+    select(icon) {
+      this.$emit('select', {
+        id: icon.id,
+        name: icon.name,
+        pack: this.iconPack.id,
+      });
+    },
+  },
+};
+</script>
--- src/components/iconpicker/IconPickerModal.vue
+++ src/components/iconpicker/IconPickerModal.vue
@@ -19,47 +19,34 @@
         v-for="iconPack in iconPacks"
         :key="iconPack.id"
         :label="iconPack.name">
-        <button
-          v-for="icon in (search.length > 0 ? visibleIcons : iconPack.icons)"
-          :key="icon.id"
-          :class="{'is-primary': icon.id === value.id, 'is-transparent': icon.id !== value.id}"
-          :title="icon.name"
-          type="button"
-          class="button"
-          @click="() => select(icon.id, iconPack.id, icon.name)">
-          <b-icon
-            :pack="iconPack.id"
-            :icon="icon.id"/>
-        </button>
+        <IconPickerIconList
+          :search="search"
+          :icon-pack="iconPack"
+          :value="value"
+          @select="select"/>
       </b-tab-item>
     </b-tabs>
   </div>
 </template>
 
 <script>
+import { selectedIcon } from './props';
+import IconPickerIconList from './IconPickerIconList.vue';
+
 // FIXME: tab change animation lag
 // TODO: Only show tabs if there is more than one iconPack
 // TODO: Don't remake modal everytime it's opened, use a global instance
 export default {
   name: 'IconPickerModal',
+  components: {
+    IconPickerIconList,
+  },
   props: {
     iconPacks: {
       type: Array,
       default: () => [],
     },
-    value: {
-      type: Object,
-      default() {
-        return {
-          id: '',
-          pack: '',
-        };
-      },
-      validator(value) {
-        return typeof value.id === 'string'
-            && typeof value.pack === 'string';
-      },
-    },
+    value: selectedIcon,
     title: {
       type: String,
       default: 'Choose an icon',
@@ -71,39 +58,14 @@ export default {
       search: '',
     };
   },
-  computed: {
-    visibleIcons() {
-      const { icons } = this.iconPacks[this.activeTab];
-      if (this.search && this.search.length > 0) {
-        return icons.filter((icon) => {
-          const toMatch = ['id', 'name', 'filter', 'categories'];
-          for (const prop of toMatch) {
-            if (icon[prop]) {
-              if (typeof icon[prop] === 'string' && icon[prop].toLowerCase().includes(this.search)) {
-                return true;
-              } else if (Array.isArray(icon[prop])) {
-                const regex = new RegExp(this.search, 'i');
-                const match = regex.test(icon[prop].join('|'));
-                if (match) {
-                  return true;
-                }
-              }
-            }
-          }
-          return false;
-        });
-      }
-      return icons;
-    },
-  },
   mounted() {
     this.$refs.search.focus();
   },
   methods: {
     searchChange(value) {
-      this.search = value.toLowerCase();
+      this.search = value;
     },
-    select(id, pack, name) {
+    select({ id, pack, name }) {
       this.$emit('input', { id, pack, name });
       if ('close' in this.$parent && typeof this.$parent.close === 'function') {
         this.$parent.close();
