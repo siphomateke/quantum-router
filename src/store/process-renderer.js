ea7ceebd6121eb954d7a86f54c45bbc371e51ad1
siphomateke <siphomateke@gmail.com>
2019-02-28 11:52:35 +0200

    Strip actions from renderer store

--- src/store/process-renderer.js
+++ src/store/process-renderer.js
@@ -1,4 +1,19 @@
 import storeOptions from '@/store';
 import Vuex from 'vuex';
 
-export default new Vuex.Store(storeOptions);
+/**
+ * @param {import('vuex').StoreOptions} storeOptions
+ * @returns {import('vuex').StoreOptions}
+ */
+function removeActionsFromStore(storeOptions) {
+  delete storeOptions.actions;
+  if (storeOptions.modules) {
+    for (const moduleName of Object.keys(storeOptions.modules)) {
+      storeOptions.modules[moduleName] = removeActionsFromStore(storeOptions.modules[moduleName]);
+    }
+  }
+  return storeOptions;
+}
+
+const strippedOptions = Object.assign({}, storeOptions);
+export default new Vuex.Store(removeActionsFromStore(strippedOptions));
